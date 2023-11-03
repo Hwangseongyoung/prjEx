15295c24dd9599f70ad34ab588dcf9bb2579baae
Hwangseongyoung <dfghukl1333@naver.com>
2023-11-04 03:02:44 +0900

    prjEx1104

---
+++ js/main_item.js
@@ -0,0 +1,35 @@
+document.querySelector(".facil-item-img1").addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  document.querySelector(".facil-item-name1").style.display = "block";
+});
+document.querySelector(".facil-item-img1").addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  document.querySelector(".facil-item-name1").style.display = "none";
+});
+
+document.querySelector(".facil-item-img2").addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  document.querySelector(".facil-item-name2").style.display = "block";
+});
+document.querySelector(".facil-item-img2").addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  document.querySelector(".facil-item-name2").style.display = "none";
+});
+
+document.querySelector(".facil-item-img3").addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  document.querySelector(".facil-item-name3").style.display = "block";
+});
+document.querySelector(".facil-item-img3").addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  document.querySelector(".facil-item-name3").style.display = "none";
+});
+
+document.querySelector(".facil-item-img4").addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  document.querySelector(".facil-item-name4").style.display = "block";
+});
+document.querySelector(".facil-item-img4").addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  document.querySelector(".facil-item-name4").style.display = "none";
+});
---
+++ js/main_nav.js
@@ -0,0 +1,35 @@
+document.querySelector(".menu1").addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  document.querySelector(".m-sub1").style.display = "block";
+});
+document.querySelector(".menu1").addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  document.querySelector(".m-sub1").style.display = "none";
+});
+
+document.querySelector(".menu2").addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  document.querySelector(".m-sub2").style.display = "block";
+});
+document.querySelector(".menu2").addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  document.querySelector(".m-sub2").style.display = "none";
+});
+
+document.querySelector(".menu3").addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  document.querySelector(".m-sub3").style.display = "block";
+});
+document.querySelector(".menu3").addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  document.querySelector(".m-sub3").style.display = "none";
+});
+
+document.querySelector(".menu5").addEventListener('mouseover', function(e) {
+  e.preventDefault();
+  document.querySelector(".m-sub5").style.display = "block";
+});
+document.querySelector(".menu5").addEventListener('mouseleave', function(e) {
+  e.preventDefault();
+  document.querySelector(".m-sub5").style.display = "none";
+});
